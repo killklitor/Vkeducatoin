a172c8fb6a554b001dce486e2b1902ae7195c388
killklitor <158448112+killklitor@users.noreply.github.com>
2024-03-18 13:41:09 +0300

    домашнее задание 12
    домашнее задание 12

--- solution (1).py
+++ solution (1).py
@@ -1,2 +1,9 @@
-username=(input())
-print("Hello,",username)
+def average_value(input_string):
+    numbers = [int(n) for n in input_string.split() if n.isdigit()]
+    return sum(numbers) / len(numbers) if numbers else None
+
+while True:
+    line = input()
+    if not line.strip():
+        break
+    print(average_value(line))
